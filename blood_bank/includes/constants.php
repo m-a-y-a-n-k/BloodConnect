70cac606c28abf538181e1585dc183d11313b4d2
Mayank Narula <mayanknarula96@gmail.com>
2016-07-13 20:17:59 +0530

    Update constants.php

--- blood_bank/includes/constants.php
+++ blood_bank/includes/constants.php
@@ -3,7 +3,7 @@
 // Database constant
 define("DB_SERVER","localhost");
 define("DB_USER","root");
-define("DB_PASS","Ilovemystery");
+define("DB_PASS","[your_password]");
 define("DB_NAME","corp");
 
 ?>
