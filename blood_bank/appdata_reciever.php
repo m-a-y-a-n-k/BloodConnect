62ce902f9118be06623b127223e90c5a3ea100ea
Mayank Narula <mayanknarula96@gmail.com>
2016-07-15 13:48:22 +0530

    Add files via upload

---
+++ blood_bank/appdata_reciever.php
@@ -0,0 +1,13 @@
+<?php
+	require_once("includes/connection.php");
+?>
+
+<?php
+	require_once("includes/functions.php");
+?>
+
+<?php
+	if( $_POST ) {
+		save_data_from_app();
+	}
+?>
---
+++ blood_bank/appdata_sender.php
@@ -0,0 +1,11 @@
+<?php
+	require_once("includes/connection.php");
+?>
+
+<?php
+	require_once("includes/functions.php");
+?>
+
+<?php
+	send_all_data_to_app();
+?>
